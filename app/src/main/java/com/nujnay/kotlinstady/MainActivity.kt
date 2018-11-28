825b8cf700847819dda05391f18d07592ce812f3
8937 <yanjun@adpanshi.com>
2018-11-28 17:36:50 +0800

    master

--- app/src/main/java/com/nujnay/kotlinstady/MainActivity.kt
+++ app/src/main/java/com/nujnay/kotlinstady/MainActivity.kt
@@ -13,10 +13,20 @@ class MainActivity : Activity() {
         var acc = Account()
         acc add 100.00
         Log.d("onCreateonCreate", acc.balance.toString())
+        val l = mutableListOf(1, 2, 3)
+        Log.d("mutableListOfd", l.toString())
+        l.swap(0, 2) // “swap()”内部的“this”得到“l”的值
+        Log.d("mutableListOfd", l.toString())
 
     }
 
     infix fun Int.add(x: Int): Int {
         return this + x
+    }
+
+    fun MutableList<Int>.swap(index1: Int, index2: Int) {
+        val tmp = this[index1] // “this”对应该列表
+        this[index1] = this[index2]
+        this[index2] = tmp
     }
 }
