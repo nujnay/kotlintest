4642d6a10f01f6ced3dc6d584ea271a3f445204a
8937 <yanjun@adpanshi.com>
2018-12-15 14:57:51 +0800

    11

--- app/src/main/java/com/nujnay/kotlinstady/MainActivity.kt
+++ app/src/main/java/com/nujnay/kotlinstady/MainActivity.kt
@@ -3,6 +3,9 @@ package com.nujnay.kotlinstady
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 
 class MainActivity : Activity() {
 
@@ -18,6 +21,26 @@ class MainActivity : Activity() {
         l.swap(0, 2) // “swap()”内部的“this”得到“l”的值
         Log.d("mutableListOfd", l.toString())
 
+
+        for (num in 0..2) {
+            Log.d("numnum", num.toString())
+        }
+        for (num in 0 until 2) {
+            Log.d("numnum||", num.toString())
+        }
+
+        for (num in 1 until 2) {
+            Log.d("numnum++", num.toString())
+        }
+        val x = 1
+        when (x) {
+            1 -> {
+            }
+            2 -> {
+            }
+            else -> {
+            }
+        }
     }
 
     infix fun Int.add(x: Int): Int {
